2ee3a2c5cde1590b7faad12dc9c7a7e6bf2559ae
Kate-Shepel <kateborichevskaya@gmail.com>
2024-10-05 20:40:25 +0200

    feat: add solution for list task (file system)

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { stat, readdir } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const sourceFolderPath = join(__dirname, 'files');
+const errorMsg = 'FS operation failed';
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const sourseFolderInfo = await stat(sourceFolderPath);
+
+        if (!sourseFolderInfo.isDirectory()) {
+            throw new Error(errorMsg);
+        }
+
+        const filesToLog = await readdir(sourceFolderPath);
+
+        console.log(filesToLog);
+    } catch (err) {
+        throw new Error(errorMsg);
+    }
 };
 
 await list();
