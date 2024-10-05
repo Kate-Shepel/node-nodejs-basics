0ceb1e18cd4177e56c0b07038067d57fb57e5296
Kate-Shepel <kateborichevskaya@gmail.com>
2024-10-05 21:46:10 +0200

    feat: add solution for read task (file system)

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { readFile } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileToRead = join(__dirname, 'files', 'fileToRead.txt');
+const errorMsg = 'FS operation failed';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const fileToReadContent = await readFile(fileToRead, 'utf-8');
+
+        console.log(fileToReadContent);
+    } catch (err) {
+        throw new Error(errorMsg);
+    }
 };
 
 await read();
