619aa002ffd8b790493f78d1a43ed47e461ed16e
Kate-Shepel <kateborichevskaya@gmail.com>
2024-10-05 23:22:04 +0200

    feat: add solution for delete task (file system)

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { unlink } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileToRemove = join(__dirname, 'files', 'fileToRemove.txt');
+const errorMsg = 'FS operation failed';
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await unlink(fileToRemove);
+        console.log(`${fileToRemove} has been successfully deleted.`);
+    } catch (err) {
+        throw new Error(errorMsg);
+    }
 };
 
 await remove();
