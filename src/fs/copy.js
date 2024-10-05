f7199633c3fa29c587bdb316617c7197689b0fb6
Kate-Shepel <kateborichevskaya@gmail.com>
2024-10-05 16:12:53 +0200

    feat: add solution for copy task (file system)

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { readdir, mkdir, copyFile } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const sourceDir = join(__dirname, 'files');
+const targetDir = join(__dirname, 'files_copy');
+const errorMsg = 'FS operation failed';
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const filesToBeCopied = await readdir(sourceDir);
+        console.log(filesToBeCopied);
+        await mkdir(targetDir);
+        await Promise.all(
+            filesToBeCopied.map(file =>
+            copyFile(join(sourceDir, file), join(targetDir, file))
+        ));
+    } catch (err) {
+        throw new Error(errorMsg);
+    }
 };
 
 await copy();
