2a7c953b72e748230e6fae5fea6d88a396a0c502
Kate-Shepel <kateborichevskaya@gmail.com>
2024-10-06 20:16:31 +0200

    feat: add solution for read task (Streams)

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,28 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileToReadPath = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const fileReadStream = createReadStream(fileToReadPath, 'utf-8');
+        fileReadStream.pipe(stdout);
+
+        fileReadStream.on('end', () => {
+            console.log('\n\n-----File reading has been completed.-----');
+        });
+
+        fileReadStream.on('error', (error) => {
+            console.error('File reading error:', error);
+        });
+    } catch (error) {
+        console.error('Error:', error);
+    }
 };
 
 await read();
