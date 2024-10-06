c2d06866828209ec016abb500790572e56362941
Kate-Shepel <kateborichevskaya@gmail.com>
2024-10-06 14:01:41 +0200

    feat: add solution for args task (Command line interface)

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    const result = [];
+    const commandLineArgs = process.argv.slice(2);
+
+    for (let i = 0; i < commandLineArgs.length; i += 2) {
+        const property = commandLineArgs[i].replace('--', '');
+        const propValue = commandLineArgs[i + 1];
+        result.push(`${property} is ${propValue}`);
+    }
+
+    console.log(result.join(', '));
 };
 
 parseArgs();
