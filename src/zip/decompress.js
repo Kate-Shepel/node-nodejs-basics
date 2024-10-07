944cbcf6967f2296b32432f16c1214f41b016795
Kate-Shepel <kateborichevskaya@gmail.com>
2024-10-07 14:09:11 +0200

    feat: add solution for decompress task (Zlib)

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,41 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createGunzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const archiveFilePath = join(__dirname, 'files', 'archive.gz');
+const decompressedFilePath = join(__dirname, 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        const readingStream = createReadStream(archiveFilePath);
+        const writingStream = createWriteStream(decompressedFilePath);
+        const decompressionStream = createGunzip();
+
+        readingStream.pipe(decompressionStream).pipe(writingStream);
+
+        writingStream.on('finish', () => {
+            console.log('---File has been decompressed successfully.---');
+        });
+
+        readingStream.on('error', (error) => {
+            console.error('Archive reading error:', error);
+        });
+
+        writingStream.on('error', (error) => {
+            console.error('File writing error:', error);
+        });
+
+        decompressionStream.on('error', (error) => {
+            console.error('Decompression error:', error);
+        });
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
 };
 
 await decompress();
